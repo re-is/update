d3251a2144cf4c8c357c970c62c4bbe751a5bfd2
re-is <33784291+re-is@users.noreply.github.com>
2019-12-25 18:52:33 +0100

    Update PC-Apps.js

--- PC-Apps.js
+++ PC-Apps.js
@@ -1,17 +1,15 @@
-// --- App Center ---//
-
 window.pcApps = {
-	'Any To Icon':
-	'AutoIt':
-	'CCleaner':
-	'Defrag':
-	'KeepAwake':
-	'Photo Filter 10':
-	'Resource Hacker':
-	'Sublime Text 3':
-	'uTorrent':
-	'Volumouse':
-	'WinRAR':
-	'WinSCP':
-	'WU Switcher':
+	'Any To Icon': ['https://www.dropbox.com/s/v8g9ot3ar4xab8e/Any.zip?dl=0', '12.25'],
+	'AutoIt': [],
+	'CCleaner': [],
+	'Defrag': [],
+	'KeepAwake': [],
+	'Photo Filter 10': [],
+	'Resource Hacker': [],
+	'Sublime Text 3': [],
+	'uTorrent': [],
+	'Volumouse': [],
+	'WinRAR': [],
+	'WinSCP': [],
+	'WU Switcher': []
 };
