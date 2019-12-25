15537bfb50b2419f3d685424093797560c2e1bfd
re-is <33784291+re-is@users.noreply.github.com>
2019-12-25 18:48:35 +0100

    Add files via upload

---
+++ PC-Apps.js
@@ -0,0 +1,17 @@
+// --- App Center ---//
+
+window.pcApps = {
+	'Any To Icon':
+	'AutoIt':
+	'CCleaner':
+	'Defrag':
+	'KeepAwake':
+	'Photo Filter 10':
+	'Resource Hacker':
+	'Sublime Text 3':
+	'uTorrent':
+	'Volumouse':
+	'WinRAR':
+	'WinSCP':
+	'WU Switcher':
+};
